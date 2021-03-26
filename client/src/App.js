be14d4f6620e65022ab527dd73c67c4448641a5c
Jason McDonald <jasonmcd65@gmail.com>
2021-03-25 20:51:07 -0500

    routes for Sign in, up, and 404 error

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Component } from 'react';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
+import Error404 from './Components/Error404'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path={"/"} component={SignIn} />
+          <Route exact path={"/SignUp"} component={SignUp} />
+          <Route path={"*"} component={Error404} />
+        </Switch>
+      </Router>
     </div>
   );
 }
