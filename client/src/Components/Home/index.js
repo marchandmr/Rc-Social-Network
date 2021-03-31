8975b5b77a5c79ee81efe592c5ec11f58d49a8c5
Jason McDonald <jasonmcd65@gmail.com>
2021-03-30 20:51:14 -0500

    preliminary logout button

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React, {Component, useEffect} from "react"
+import LogoutButton from "../LogoutButton"
 import PostList from "../PostList"
+
 
 function Home(){
 
+    useEffect(() =>{
+        console.log("Content loaded")
+
+
+    })
 
     return(
         <div>
              <h1>RC Spots</h1>
+             <LogoutButton/>
              <p>Hello {localStorage.getItem("currentUsername")} </p>
              <p>Your email is {localStorage.getItem("currentEmail")}</p>
              <h2>Post list goes under here</h2>
