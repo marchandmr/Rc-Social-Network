b141aaad44dda34590180637d6cf2e5e92532226
Jason McDonald <jasonmcd65@gmail.com>
2021-04-01 18:54:17 -0500

    axios

--- client/src/Components/SignIn/index.js
+++ client/src/Components/SignIn/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import API from "../../utils/API"
 
 function SignIn(props) {
 
@@ -28,8 +29,31 @@ function SignIn(props) {
         // send information to server with passport and switch 
         // somehow with userid data?
         // dont forget to add username from login response
+
+        //  API.signup({
+        //     username: username,
+        //     email: email,
+        //     password: password
+        // }).then(response => {
+        //     if(response.data){
+        //         alert("Sign Up Successful")
+        //         window.location.href="/"
+        //     }
+        // }).catch(error => {
+        //     console.log("Error: ", error)
+        // })
+
+        API.login({
+            email: email,
+            password:password
+        }).then(response =>{
+            if(response.status === 200 ){
+                console.log(response.data)
+            }
+        })
+
         props.update("Maya", email)
-        window.location.href="/Home"
+        // window.location.href="/Home"
         
     }
 
