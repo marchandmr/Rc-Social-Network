fae90798302c86dae4e02476402c11198f2be6ab
Oscar Medina <ogmedina04@gmail.com>
2021-04-05 14:07:38 -0500

    moved file structure around, still working on getting image model integrated with user model

--- routes/api/uploadImageRoute.js
+++ routes/api/uploadImageRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { UploadImage } = require("../../controllers/imageController");
-const parser = require("../../middleware/cloudinary.config");
+const parser = require("../../config/middleware/cloudinary.config");
 
 router.post("/image", parser.single("image"), UploadImage);
 
