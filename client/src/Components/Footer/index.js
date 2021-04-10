1f96bc214080b69013dc1445946b62a6d9174574
Matthew Marchand <marchandmr@icloud.com>
2021-04-10 12:05:51 -0500

    re uploading footer

---
+++ client/src/Components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./footer.css"
+const Footer = () => (
+    <div className="footer">
+        <p className="footerText">© Copyright 2021 RC Spots</p>
+    </div>
+);
+
+export default Footer;
