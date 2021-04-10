1abee5a74db95875eaf30a719d882ee0d3bc7005
Jason McDonald <jasonmcd65@gmail.com>
2021-04-10 13:12:08 -0500

    removed console logs and unused packages

--- client/src/Components/CreatePostModal/index.js
+++ client/src/Components/CreatePostModal/index.js
@@ -12,8 +12,6 @@ import { Link } from "react-router-dom"
 function CreatePostModal(props) {
 
     const [formObject, setFormObject] = useState({})
-
-    //Image Upload Code---------------------->
     const [fileData, setFileData] = useState();
     const [images, setFile] = useState("");
 
@@ -32,7 +30,6 @@ function CreatePostModal(props) {
             let lat = position.coords.latitude;
             let long = position.coords.longitude;
             let preciseLocString = `http://maps.google.com/maps?q=${lat},${long}`
-            console.log(preciseLocString);
             localStorage.setItem("geoLink", preciseLocString)
         }
     }
@@ -41,14 +38,11 @@ function CreatePostModal(props) {
     const handleSubmit = async () => {
 
         const formdata = new FormData();
-
         formdata.append('image', fileData);
-
         // if there is no image to upload, skip cloudinary
         if (!fileData) {
             localStorage.setItem("imageLink", "")
             props.submitPost(formObject)
-
         } else { //otherwise there is an image to upload
             API.createImage(formdata)
                 .then((res) => {
@@ -56,7 +50,6 @@ function CreatePostModal(props) {
                     props.submitPost(formObject)
                 })
                 .catch((error) => console.error(error));
-
         }
     };
 
@@ -66,58 +59,50 @@ function CreatePostModal(props) {
         setFormObject({ ...formObject, [name]: value })
     }
 
-
-    
-    return(
+    return (
         <>
-        <Container className="postWrapper">
-    <div id="postModal" className="wrapper">
-        <h2>Create a Post</h2>
-        <Form>
-            <Container className="formArea">
-            <Form.Row>                    
-            <Form.Control as="textarea" rows={4} placeholder="Description" name="body"
-            onChange={handleInputChange}
-            />
-            
-           </Form.Row>
-           <br />
-           <Form.Row className="cityForm">
-            <input type="text" placeholder="City" name="city"
-            onChange={handleInputChange}
-            />
-           
-            {/* state selector tx-ks-ark */}
-            <StateSelector handleInputChange={handleInputChange}/>
-            </Form.Row>
-
-            {/* image uploader */}
-            <Form.Row>
-            <Form.File
-                className='fileUpload'
-                type='file'
-                value={images}
-                name='file'
-                accept="image/*"
-                onChange={handleFileChange}
-                placeholder='upload image'
-                // isRequired={true}
-            />
-            </Form.Row>
-            <br />
-            
-            <Link to="/Home"><Button className="gobackBtn" variant="danger" onClick={() => props.updateShowCreate(false)}><i className="fas fa-arrow-alt-circle-left fa-lg"></i> Go Back</Button></Link>
-            <Button className="locationBtn" variant="success" onClick={() => getPreciseLoc()}><i className="fas fa-location-arrow fa-lg"></i> Share Location</Button>
-            <Button className="submitBtn" onClick={() => handleSubmit()}><i className="fas fa-upload fa-lg"></i> Submit</Button>
-            
+            <Container className="postWrapper">
+                <div id="postModal" className="wrapper">
+                    <h2>Create a Post</h2>
+                    <Form>
+                        <Container className="formArea">
+                            <Form.Row>
+                                <Form.Control as="textarea" rows={4} placeholder="Description" name="body"
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Row>
+                            <br />
+                            <Form.Row className="cityForm">
+                                <input type="text" placeholder="City" name="city"
+                                    onChange={handleInputChange}
+                                />
+                                {/* state selector tx-ks-ark */}
+                                <StateSelector handleInputChange={handleInputChange} />
+                            </Form.Row>
+                            {/* image uploader */}
+                            <Form.Row>
+                                <Form.File
+                                    className='fileUpload'
+                                    type='file'
+                                    value={images}
+                                    name='file'
+                                    accept="image/*"
+                                    onChange={handleFileChange}
+                                    placeholder='upload image'
+                                />
+                            </Form.Row>
+                            <br />
+                            <Link to="/Home"><Button className="gobackBtn" variant="danger" onClick={() => props.updateShowCreate(false)}><i className="fas fa-arrow-alt-circle-left fa-lg"></i> Go Back</Button></Link>
+                            <Button className="locationBtn" variant="success" onClick={() => getPreciseLoc()}><i className="fas fa-location-arrow fa-lg"></i> Share Location</Button>
+                            <Button className="submitBtn" onClick={() => handleSubmit()}><i className="fas fa-upload fa-lg"></i> Submit</Button>
+                        </Container>
+                    </Form>
+                    <br />
+                </div>
             </Container>
-        </Form>
-        <br />
-        </div>
-        </Container>
         </>
     )
-    
+
 }
 
 export default CreatePostModal
--- client/src/Components/Jumbotron/index.js
+++ client/src/Components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
     return (
         <div
-            style={{background: "#003566", color: "white", height: 300, paddingTop: 120, textAlign: "center"}}
+            style={{ background: "#003566", color: "white", height: 300, paddingTop: 120, textAlign: "center" }}
             className="jumbotron"
         >
             {children}
