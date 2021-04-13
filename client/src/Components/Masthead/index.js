30db8f447e4100fac3ab729fe732fdc454e1e43e
Oscar Medina <ogmedina04@gmail.com>
2021-04-12 20:17:21 -0500

    debug header

--- client/src/Components/Masthead/index.js
+++ client/src/Components/Masthead/index.js
@@ -6,11 +6,7 @@ function Masthead() {
     return (
         <>
         <header className="masthead">
-            <Container>
-                {/* <div className="masthead-heading text-uppercase"></div> */}
-                {/* <div className="masthead-subheading"></div> */}
-                {/* <button className="btn btn-primary btn-x1 text-uppercase">Make a Post</button> */}
-            </Container>
+    
         </header>    
         </>
     )
