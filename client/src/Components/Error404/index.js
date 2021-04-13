fff964700ceafe16beb1679509ced3874b9d7a53
Jason McDonald <jasonmcd65@gmail.com>
2021-04-12 20:02:28 -0500

    designed 404 page

--- client/src/Components/Error404/index.js
+++ client/src/Components/Error404/index.js
@@ -1,11 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
+import "./404.css"
+import SignupJumbotron from "../SignupJumbotron"
+import Footer from "../Footer";
+import Container from "react-bootstrap/Container"
 
 function Error404() {
 
-    
-    return(
+
+    return (
         <div>
-            <h1>404</h1>
+            <SignupJumbotron />
+            <Container className="lettering">
+
+
+                <div className="">
+                    <h2 className="lettering">Error 404</h2>
+                    <h3>Looks like you bashed your RC car a little too hard!</h3>
+                    <hr></hr>
+                    <h4>Click the back button in your browser to go back to RC Spots!</h4>
+                </div>
+                <div>
+                   <img src="./carfire.jpg"/>
+                    
+                </div>
+            </Container>
+            <Footer />
         </div>
     )
 }
